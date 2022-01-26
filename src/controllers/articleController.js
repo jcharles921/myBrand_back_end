45fdd06c0ba27a79936031b007653011e131df6a
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2022-01-26 21:07:40 +0200

    add the crud opps

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,8 +1,36 @@
+import { createArticleService, getAllArticlesService, getOneArticleService } from "../services/articleServices.js"
 export class ArticleController {
-    // TODO Don't access database from this file you only needs
-    createArticle(req, res, next) { }
-    getAllArticles(req, res, next) { }
-    getArticle(req, res, next) { }
+    async createArticle(req, res, next) {
+        try {
+            const data = {
+                title: req.body.title,
+                content: req.body.content,
+                image: req.body.image,
+                create_at: new Date()
+            }
+            console.log(data)
+            const article = await createArticleService(data)
+            res.status(200).json({ status: 200, message: "Article created successfully", data: article })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async getAllArticles(req, res, next) {
+        try {
+            const articles = await getAllArticlesService()
+            res.status(200).json({ status: 200, message: "These are all the articles", data: articles })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async getArticle(req, res, next) {
+        try {
+            const article = await getOneArticleService(req.params.id)
+            res.status(200).json({ status: 200, message: "article retieved successfully", data: article })
+        } catch (error) {
+            console.log(error)
+        }
+    }
     updateArticle(req, res, next) { }
     deleteArticle(req, res, next) { }
 }
--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,8 +1,17 @@
-// import the model you need to access
-export class ArticleServices {
-    createArticle(data) { }
-    getAllArticles() { }
-    getArticle(id) { }
-    updateArticle(id) { }
-    deleteArticle(id) { }
+import Article from "../models/article.js";
+
+export const createArticleService = async (data) => {
+    const article = await Article(data)
+    article.save()
+    return article
+}
+
+export const getAllArticlesService = async () => {
+    const articles = await Article.find()
+    return articles
+}
+
+export const getOneArticleService = async (id) => {
+    const article = await Article.findOne({ _id: id })
+    return article
 }
